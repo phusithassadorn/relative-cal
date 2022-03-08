c9b1adb504985a588e75ee1c4e21d1d1e8b251f0
Phusit Hassadorn <68945774+phusithassadorn@users.noreply.github.com>
2022-03-08 09:42:04 +0700

    fixed

--- relative-cal.py
+++ relative-cal.py
@@ -4,3 +4,8 @@ import pandas as pd
 
 #ใช้ฟังก์ชัน read_csv จากไลบรารี pandas อ่านข้อมูลจากไฟล์ชื่อ predator_dataset.csv แล้วในมาเก็บไว้ใน data frame ชื่อ data
 data = pd.read_csv('predator_dataset.csv')
+#คำนวณค่าความสัมพันธ์ โดยใช้ฟังก์ชัน linregress จากไลบรารี scipy
+slope,intercept,_,_,_ = stats.linregress(data['Wildebeest'], data['Zebra'])
+#แสดงผลลัพธ์
+print("slope = ",slope)
+print("intercept = ",intercept)
